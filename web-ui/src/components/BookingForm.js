30a55d2384c0ddbe29204da2cbf3a9e78c1f4abf
Makendy Midouin <makendymidouin99@gmail.com>
2023-08-10 02:26:03 -0400

    new changes and cleaned code

--- web-ui/src/components/BookingForm.js
+++ web-ui/src/components/BookingForm.js
@@ -28,28 +28,6 @@ const defaultTheme = createTheme();
 
 const names = ["Laundry and Folding", "Oven", "Fridge", "Baseboards"];
 const focusedColor = "#8C52FF";
-// const useStyles = makeStyles({
-//   root: {
-//     // input label when focused
-//     "& label.Mui-focused": {
-//       color: focusedColor,
-//     },
-//     // focused color for input with variant='standard'
-//     "& .MuiInput-underline:after": {
-//       borderBottomColor: focusedColor,
-//     },
-//     // focused color for input with variant='filled'
-//     "& .MuiFilledInput-underline:after": {
-//       borderBottomColor: focusedColor,
-//     },
-//     // focused color for input with variant='outlined'
-//     "& .MuiOutlinedInput-root": {
-//       "&.Mui-focused fieldset": {
-//         borderColor: focusedColor,
-//       },
-//     },
-//   },
-// });
 
 const Containers = styled.div`
   width: 475px;
