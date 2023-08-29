a1fd15bd10dbc66bf2d9df00cde8d8a0a349c41b
Makendy Midouin <makendymidouin99@gmail.com>
2023-08-29 03:52:21 -0400

    Made the header sticky

--- web-ui/src/layout/Example.js
+++ web-ui/src/layout/Example.js
@@ -6,7 +6,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { Link } from "react-router-dom"; // Import the Link component from react-router-dom
-import GlobalStyles from "@mui/material/GlobalStyles";
 import Box from "@mui/material/Box";
 import logo from "../assets/Quick&Fast-Logo.png";
 import { useNavigate } from "react-router-dom";
@@ -25,67 +24,66 @@ export default function Example() {
 
   return (
     <ThemeProvider theme={customTheme}>
-      <GlobalStyles
-        styles={{ ul: { margin: 0, padding: 0, listStyle: "none" } }}
-      />
-      <CssBaseline />(
-      <AppBar
-        position="static"
-        color="default"
-        elevation={0}
-        sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
-      >
-        <Toolbar sx={{ flexWrap: "wrap" }}>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-start",
-              flexGrow: 1,
-              gap: "20px",
-            }}
-          >
-            <Link to="/" style={{ textDecoration: "none" }}>
-              <img
-                src={logo}
-                alt="Logo"
-                style={{
-                  width: "100px",
-                  height: "100px",
-                  borderRadius: "50px",
-                }}
-              />
-            </Link>
-            <Box>
-              <Typography variant="h6" color="inherit" noWrap>
-                Quick &amp; Fast
-              </Typography>
-              <Typography variant="subtitle1" color="inherit" noWrap>
-                Cleaning Services
-              </Typography>
+      <CssBaseline />
+      <Box sx={{ position: "sticky", top: 0, zIndex: 100 }}>
+        <AppBar
+          position="static"
+          color="default"
+          elevation={0}
+          sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+        >
+          <Toolbar sx={{ flexWrap: "wrap" }}>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-start",
+                flexGrow: 1,
+                gap: "20px",
+              }}
+            >
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <img
+                  src={logo}
+                  alt="Logo"
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                    borderRadius: "50px",
+                  }}
+                />
+              </Link>
+              <Box>
+                <Typography variant="h6" color="inherit" noWrap>
+                  Quick &amp; Fast
+                </Typography>
+                <Typography variant="subtitle1" color="inherit" noWrap>
+                  Cleaning Services
+                </Typography>
+              </Box>
             </Box>
-          </Box>
 
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-end",
-            }}
-          >
-            <Box sx={{ my: 1, mx: 1 }}>
-              <Button
-                onClick={BookingPage}
-                style={{ color: "#8C52FF" }}
-                color="inherit"
-                variant="outlined"
-              >
-                Book Now
-              </Button>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+              }}
+            >
+              <Box sx={{ my: 1, mx: 1 }}>
+                <Button
+                  onClick={BookingPage}
+                  style={{ color: "#8C52FF" }}
+                  color="inherit"
+                  variant="outlined"
+                >
+                  Book Now
+                </Button>
+              </Box>
             </Box>
-          </Box>
-        </Toolbar>
-      </AppBar>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </ThemeProvider>
   );
 }
